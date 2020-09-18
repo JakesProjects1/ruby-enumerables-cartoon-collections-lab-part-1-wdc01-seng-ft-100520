effbe5a692a028a8f04c851851b16c1377603523
Jacob Kagon <jacob@kagon.net>
2020-09-18 01:45:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   array.each_with_index do |index, item|
    item_plus_one = item + 1
-   puts "index ,#{index_plus_one}. item"
+   puts "index ,#{item_plus_one}. item"
    puts index
   end
 end
